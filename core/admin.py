ea35588ce302394f67f3513d5151a6abed3650c2
faisalburhanudin <faisalburhanudin@gmail.com>
2017-04-10 02:02:09 +0700

    show title and image

--- core/admin.py
+++ core/admin.py
@@ -12,6 +12,14 @@ def avatar_tag(obj):
 class UserAdmin(admin.ModelAdmin):
     list_display = ('name', avatar_tag)
 
+
+def content_img_tag(obj):
+    return mark_safe('<img src="%s" width="150" height="150" />' % obj.image_50x50.url)
+
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('title', content_img_tag)
+
 # Register your models here.
 admin.site.register(User, UserAdmin)
-admin.site.register(Content)
+admin.site.register(Content, ContentAdmin)
