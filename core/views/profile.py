4678b2deb5accd35c7452b4b297158167b49bdad
faisalburhanudin <faisalburhanudin@gmail.com>
2017-05-21 22:16:29 +0700

    user profile

--- core/views/profile.py
+++ core/views/profile.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from core.models import Content
+
 
 def profile_view(request):
     """Home profile user"""
-    return render(request, "profile.html")
+    user = request.user
+    contents = Content.objects.filter(author=user).all()
+    return render(request, "profile.html", {"user": user, "contents": contents})
