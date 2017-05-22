42afe2ddbc5fa8123573a6b12373a3b69217069b
faisalburhanudin <faisalburhanudin@gmail.com>
2017-05-22 22:04:36 +0700

    change navbar, add profile

--- core/views/profile.py
+++ core/views/profile.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 from core.models import Content
 
 
 def profile_view(request):
     """Home profile user"""
-    user = request.user
+    username = request.GET.get("username")
+    if username:
+        user = User.objects.get(username=username)
+    else:
+        user = request.user
+
     contents = Content.objects.filter(author=user).all()
     return render(request, "profile.html", {"user": user, "contents": contents})
