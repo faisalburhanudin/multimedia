9f32394be79a7dd159cbd36ec9a86ebe185fce02
faisalburhanudin <faisalburhanudin@gmail.com>
2017-05-23 22:20:14 +0700

    comment list

--- core/views/content.py
+++ core/views/content.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
-from core.models import Content
+from core.models import Content, UserComment
 
 
 def content_view(request, content_id):
     content = Content.objects.get(id=content_id)
-    return render(request, "content.html", {"content": content})
+    comments = UserComment.objects.filter(content=content)
+    return render(request, "content.html", {"content": content, "comments": comments})
