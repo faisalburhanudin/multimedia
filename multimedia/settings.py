9ec804c7886d493e63d2863138c780e89456e98c
faisalburhanudin <faisalburhanudin@gmail.com>
2017-07-15 19:52:23 +0700

    using environment variable

--- multimedia/settings.py
+++ multimedia/settings.py
@@ -121,12 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
